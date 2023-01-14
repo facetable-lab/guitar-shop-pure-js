af5a51caaa4fb762ae56263490ece85c62b23b14
facetable-lab <diofff6@gmail.com>
2023-01-14 13:06:28 +0300

    Отображение корзины

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,20 +1,22 @@
 class Header {
+    handleOpenShoppingPage() {
+        shoppingPage.render();
+    }
 
     render(count) {
         const html = `
         <div class="header-container">
-            <div class="header-counter">
+            <div class="header-counter" onclick="headerPage.handleOpenShoppingPage()">
                 🛒 ${count}
             </div>
         </div>
         `;
 
-        ROOT_HEADER.innerHTML = html
+        ROOT_HEADER.innerHTML = html;
     }
 }
 
 const headerPage = new Header();
 
 const productsStore = localStorageUtil.getProducts();
-
 headerPage.render(productsStore.length);
